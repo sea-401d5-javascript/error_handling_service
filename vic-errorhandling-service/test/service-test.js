9853ea75bfb54e37954ff16a7cf86943e5bb891b
Victor Benavente <VMB@Victors-MacBook-Air.local>
2016-06-30 10:30:48 -0700

    added tests to check for array and being able to add to array

--- vic-errorhandling-service/test/service-test.js
+++ vic-errorhandling-service/test/service-test.js
@@ -14,11 +14,20 @@ describe('service unit test', () => {
   });
 
   it('should have getErrors as function', () => {
-    console.log('are you reading me', errHandServ);
-    // expect(typeof errHandServ.getErrors).toBe('function');
+    expect(typeof errHandServ.getErrors).toBe('function');
   });
 
-  // it('should have logError as function', () => {
-  //   expect(typeof errHandServ.logError).toBe('function');
-  // });
+  it('should have logError as function', () => {
+    expect(typeof errHandServ.logError).toBe('function');
+  });
+
+  it('should get an array', () => {
+    expect(Array.isArray(errHandServ.getErrors())).toBe(true);
+  });
+
+  it('should add an error to the errors array', () => {
+    errHandServ.logError('test')();
+    let errorArray = errHandServ.getErrors();
+    expect(errorArray[0]).toBe('test');
+  });
 });
