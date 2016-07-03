0ddaed3595ab8ead0a8f090a740216e73f000e23
Stefanie Hansen <stefuhnee@gmail.com>
2016-07-03 16:57:19 -0700

    added get errors function to service, wrote out a bit of the index.html even though it's not necessary

--- stefanie-hansen/app/js/services/error-handling-service.js
+++ stefanie-hansen/app/js/services/error-handling-service.js
@@ -1,10 +1,18 @@
 module.exports = function(app) {
   app.factory('ErrorService', function() {
     const service = {};
+    const errors = [];
     service.sendErr = function(message) {
-      return {message, err};
-      console.log('Message: ' + message + ' Error: ' + err);
+      return function(err) {
+        errors.push(message);
+        console.log('error: ', err);
+      };
     };
+
+    service.getErrors = function() {
+      return errors;
+    };
+    
     return service;
   });
 };
